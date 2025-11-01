19f8a601ed3c96de097b92b8b53851c33946c2ae
Vaishak Salin <vaishak93@gmail.com>
2025-10-31 17:08:23 -0700

    Add base.py to start coding the solution

--- tests/adapters.py
+++ tests/adapters.py
@@ -8,6 +8,7 @@ import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
+from .solution import helper
 
 
 def run_linear(
@@ -589,4 +590,5 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
+    helper()
     raise NotImplementedError
---
+++ tests/solution/ base.py
@@ -0,0 +1,17 @@
+
+def helper():
+    pass
+
+class Tokenizer():
+    def __init__(self, vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]]):
+        self.vocab = vocab
+        self.merges = merges
+
+    def train(self, train_data: str):
+        raise NotImplementedError
+
+    def encode(self, input_text: str) -> list[int]:
+        raise NotImplementedError
+
+    def decode(self, token_ids: list[int]) -> str:
+        raise NotImplementedError
